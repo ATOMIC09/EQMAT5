f2c5a8a512bafb28c379d4bcb02c999b1ba6ef4c
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2024-05-22 21:34:50 +0700

    Optimized the code
    Reconstructed all function

--- EQ_STM32/Core/Src/main.c
+++ EQ_STM32/Core/Src/main.c
@@ -22,6 +22,9 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+#include <stdio.h>
+#include "arm_math.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -31,6 +34,9 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+
+#define BLOCK_SIZE_FLOAT 512
+#define BLOCK_SIZE_U16 2048
 
 /* USER CODE END PD */
 
@@ -48,6 +54,64 @@ UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 
+// UART
+uint8_t tx_buffer[17] = "Serial Ready!\n\r";
+uint8_t rx_buffer[65];
+
+// I2S
+float iir_l_state [4];
+float iir_r_state [4];
+uint16_t rxBuf[BLOCK_SIZE_U16*2];
+uint16_t txBuf[BLOCK_SIZE_U16*2];
+float l_buf_in [BLOCK_SIZE_FLOAT*2];
+float r_buf_in [BLOCK_SIZE_FLOAT*2];
+float l_buf_out [BLOCK_SIZE_FLOAT*2];
+float r_buf_out [BLOCK_SIZE_FLOAT*2];
+uint8_t callback_state = 0;
+arm_biquad_casd_df1_inst_f32 iirsettings_l, iirsettings_r;
+
+//IIR coefficient array
+// Default settings: Q = 0.1, Fs = 96kHz, Gain = 0
+float iir_coeffs_low [5] = {
+				1.000000f,
+				-1.996722f,
+				0.996733f,
+				1.996722f,
+				-0.996733
+};
+
+float iir_coeffs_lowmid [5] = {
+        1.000000f,
+        -1.986825f,
+        0.986996f,
+        1.98683f,
+        -0.986996
+};
+
+float iir_coeffs_mid [5] = {
+        1.000000f,
+        -1.897381f,
+        0.906562f,
+        1.897381f,
+        -0.906562
+};
+
+float iir_coeffs_highmid [5] = {
+        0.999999f,
+        -1.787234f,
+        0.822248f,
+        1.787234f,
+        -0.822248
+};
+
+float iir_coeffs_high [5] = {
+        0.999999f,
+        -1.216444f,
+        0.533295f,
+        1.216444f,
+        -0.533295
+};
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -57,6 +121,8 @@ static void MX_DMA_Init(void);
 static void MX_I2S2_Init(void);
 static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
+
+static void parseAndStoreCoeffs(char *rx_buffer);
 
 /* USER CODE END PFP */
 
@@ -99,6 +165,29 @@ int main(void)
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
+  // Start UART communication
+  HAL_UART_Transmit(&huart1, tx_buffer, sizeof(tx_buffer), 10); // Send ready message
+  HAL_UART_Receive_IT(&huart1, rx_buffer, sizeof(rx_buffer)); // Start UART receive
+  
+  // Start I2S communication
+  // Initialize IIR filter coefficients
+  arm_biquad_cascade_df1_init_f32 ( &iirsettings_l, 1, &iir_coeffs_low[0], &iir_l_state[0]);
+  arm_biquad_cascade_df1_init_f32 ( &iirsettings_r, 1, &iir_coeffs_low[0], &iir_r_state[0]);
+  arm_biquad_cascade_df1_init_f32 ( &iirsettings_l, 1, &iir_coeffs_lowmid[0], &iir_l_state[0]);
+  arm_biquad_cascade_df1_init_f32 ( &iirsettings_r, 1, &iir_coeffs_lowmid[0], &iir_r_state[0]);
+  arm_biquad_cascade_df1_init_f32 ( &iirsettings_l, 1, &iir_coeffs_mid[0], &iir_l_state[0]);
+  arm_biquad_cascade_df1_init_f32 ( &iirsettings_r, 1, &iir_coeffs_mid[0], &iir_r_state[0]);
+  arm_biquad_cascade_df1_init_f32 ( &iirsettings_l, 1, &iir_coeffs_highmid[0], &iir_l_state[0]);
+  arm_biquad_cascade_df1_init_f32 ( &iirsettings_r, 1, &iir_coeffs_highmid[0], &iir_r_state[0]);
+  arm_biquad_cascade_df1_init_f32 ( &iirsettings_l, 1, &iir_coeffs_high[0], &iir_l_state[0]);
+  arm_biquad_cascade_df1_init_f32 ( &iirsettings_r, 1, &iir_coeffs_high[0], &iir_r_state[0]);
+
+  // Start I2S with 2048 samples transmission -> 4096*u16 words
+  HAL_I2SEx_TransmitReceive_DMA (&hi2s2, txBuf, rxBuf, BLOCK_SIZE_U16);
+
+  int offset_r_ptr;
+  int offset_w_ptr, w_ptr;
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -108,6 +197,55 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
+    if (callback_state != 0) {
+
+		  // Decide if it was half or full cplt callback
+		  if (callback_state == 1)   {
+			  	  offset_r_ptr = 0;
+			  	  offset_w_ptr = 0;
+			  	  w_ptr = 0;
+			  }
+
+		  else if (callback_state == 2) {
+			  offset_r_ptr = BLOCK_SIZE_U16;
+			  offset_w_ptr = BLOCK_SIZE_FLOAT;
+			  w_ptr = BLOCK_SIZE_FLOAT;
+		  }
+
+
+		  // Restore input sample buffer to float array
+		  for (int i=offset_r_ptr; i<offset_r_ptr+BLOCK_SIZE_U16; i=i+4) {
+			  l_buf_in[w_ptr] = (float) ((int) (rxBuf[i]<<16)|rxBuf[i+1]);
+			  r_buf_in[w_ptr] = (float) ((int) (rxBuf[i+2]<<16)|rxBuf[i+3]);
+			  w_ptr++;
+		  }
+
+
+		  // Process IIR
+		  arm_biquad_cascade_df1_f32 (&iirsettings_l, &l_buf_in[offset_w_ptr], &l_buf_out[offset_w_ptr],BLOCK_SIZE_FLOAT);
+		  arm_biquad_cascade_df1_f32 (&iirsettings_r, &r_buf_in[offset_w_ptr], &r_buf_out[offset_w_ptr],BLOCK_SIZE_FLOAT);
+
+      // Bypass filter
+      // for (int i=offset_w_ptr; i<offset_w_ptr+BLOCK_SIZE_FLOAT; i++) {
+      //   l_buf_out[i] = l_buf_in[i];
+      //   r_buf_out[i] = r_buf_in[i];
+      // }
+
+		  //restore processed float-array to output sample-buffer
+		  w_ptr = offset_w_ptr;
+
+		  for (int i=offset_r_ptr; i<offset_r_ptr+BLOCK_SIZE_U16; i=i+4) {
+				txBuf[i] =  (((int)l_buf_out[w_ptr])>>16)&0xFFFF;
+				txBuf[i+1] = ((int)l_buf_out[w_ptr])&0xFFFF;
+				txBuf[i+2] = (((int)r_buf_out[w_ptr])>>16)&0xFFFF;
+				txBuf[i+3] = ((int)r_buf_out[w_ptr])&0xFFFF;
+				w_ptr++;
+		  }
+
+		  callback_state = 0;
+
+	  }
   }
   /* USER CODE END 3 */
 }
@@ -275,6 +413,120 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+// printf function
+int _write(int file, char *ptr, int len)
+{
+  (void)file;
+  int DataIdx;
+
+  for (DataIdx = 0; DataIdx < len; DataIdx++)
+  {
+	  ITM_SendChar(*ptr++);
+  }
+  return len;
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(huart);
+  /* NOTE: This function should not be modified, when the callback is needed,
+           the HAL_UART_RxCpltCallback could be implemented in the user file
+   */
+  // HAL_UART_Transmit(&huart1, rx_buffer, sizeof(rx_buffer), 10); // Echo the received data to serial
+  printf("Received UART: %s\n", rx_buffer); // Print the received data to serial
+  parseAndStoreCoeffs((char *)rx_buffer); // Parse the received data
+  memset(rx_buffer, 0, sizeof(rx_buffer)); // Clear the buffer
+  HAL_UART_Receive_IT(&huart1, rx_buffer, sizeof(rx_buffer)); // Start the next receive
+}
+
+static void parseAndStoreCoeffs(char *rx_buffer) {
+    // Determine which band the coefficients are for
+    if (strncmp(rx_buffer, "LowMid", 6) == 0) {
+        sscanf(rx_buffer, "LowMid %f %f %f %f %f",
+                &iir_coeffs_lowmid[0], &iir_coeffs_lowmid[1], &iir_coeffs_lowmid[2], &iir_coeffs_lowmid[3], &iir_coeffs_lowmid[4]);
+        printf("Parsed LowMid : %f %f %f %f %f\n", iir_coeffs_lowmid[0], iir_coeffs_lowmid[1], iir_coeffs_lowmid[2], iir_coeffs_lowmid[3], iir_coeffs_lowmid[4]);
+        arm_biquad_cascade_df1_init_f32 (&iirsettings_l, 1, &iir_coeffs_lowmid[0], &iir_l_state[0]);
+        arm_biquad_cascade_df1_init_f32 (&iirsettings_r, 1, &iir_coeffs_lowmid[0], &iir_r_state[0]); 
+    } else if (strncmp(rx_buffer, "HighMid", 7) == 0) {
+        sscanf(rx_buffer, "HighMid %f %f %f %f %f",
+                &iir_coeffs_highmid[0], &iir_coeffs_highmid[1], &iir_coeffs_highmid[2], &iir_coeffs_highmid[3], &iir_coeffs_highmid[4]);
+        printf("Parsed HighMid : %f %f %f %f %f\n", iir_coeffs_highmid[0], iir_coeffs_highmid[1], iir_coeffs_highmid[2], iir_coeffs_highmid[3], iir_coeffs_highmid[4]);
+        arm_biquad_cascade_df1_init_f32 (&iirsettings_l, 1, &iir_coeffs_highmid[0], &iir_l_state[0]);
+        arm_biquad_cascade_df1_init_f32 (&iirsettings_r, 1, &iir_coeffs_highmid[0], &iir_r_state[0]); 
+    } else if (strncmp(rx_buffer, "Mid", 3) == 0) {
+        sscanf(rx_buffer, "Mid %f %f %f %f %f",
+                &iir_coeffs_mid[0], &iir_coeffs_mid[1], &iir_coeffs_mid[2], &iir_coeffs_mid[3], &iir_coeffs_mid[4]);
+        printf("Parsed Mid : %f %f %f %f %f\n", iir_coeffs_mid[0], iir_coeffs_mid[1], iir_coeffs_mid[2], iir_coeffs_mid[3], iir_coeffs_mid[4]);
+        arm_biquad_cascade_df1_init_f32 (&iirsettings_l, 1, &iir_coeffs_mid[0], &iir_l_state[0]);
+        arm_biquad_cascade_df1_init_f32 (&iirsettings_r, 1, &iir_coeffs_mid[0], &iir_r_state[0]); 
+    } else if (strncmp(rx_buffer, "Low", 3) == 0) {
+        sscanf(rx_buffer, "Low %f %f %f %f %f",
+                &iir_coeffs_low[0], &iir_coeffs_low[1], &iir_coeffs_low[2], &iir_coeffs_low[3], &iir_coeffs_low[4]);
+        printf("Parsed Low : %f %f %f %f %f\n", iir_coeffs_low[0], iir_coeffs_low[1], iir_coeffs_low[2], iir_coeffs_low[3], iir_coeffs_low[4]);
+        arm_biquad_cascade_df1_init_f32 (&iirsettings_l, 1, &iir_coeffs_low[0], &iir_l_state[0]);
+        arm_biquad_cascade_df1_init_f32 (&iirsettings_r, 1, &iir_coeffs_low[0], &iir_r_state[0]); 
+    } else if (strncmp(rx_buffer, "High", 4) == 0) {
+        sscanf(rx_buffer, "High %f %f %f %f %f",
+                &iir_coeffs_high[0], &iir_coeffs_high[1], &iir_coeffs_high[2], &iir_coeffs_high[3], &iir_coeffs_high[4]);
+        printf("Parsed High : %f %f %f %f %f\n", iir_coeffs_high[0], iir_coeffs_high[1], iir_coeffs_high[2], iir_coeffs_high[3], iir_coeffs_high[4]);
+        arm_biquad_cascade_df1_init_f32 (&iirsettings_l, 1, &iir_coeffs_high[0], &iir_l_state[0]);
+        arm_biquad_cascade_df1_init_f32 (&iirsettings_r, 1, &iir_coeffs_high[0], &iir_r_state[0]);
+    } else if (strncmp(rx_buffer, "Reset", 5) == 0) {
+        iir_coeffs_low[0] = 1.000000f;
+        iir_coeffs_low[1] = -1.996722f;
+        iir_coeffs_low[2] = 0.996733f;
+        iir_coeffs_low[3] = 1.996722f;
+        iir_coeffs_low[4] = -0.996733;
+
+        iir_coeffs_lowmid[0] = 1.000000f;
+        iir_coeffs_lowmid[1] = -1.986825f;
+        iir_coeffs_lowmid[2] = 0.986996f;
+        iir_coeffs_lowmid[3] = 1.98683f;
+        iir_coeffs_lowmid[4] = -0.986996;
+
+        iir_coeffs_mid[0] = 1.000000f;
+        iir_coeffs_mid[1] = -1.897381f;
+        iir_coeffs_mid[2] = 0.906562f;
+        iir_coeffs_mid[3] = 1.897381f;
+        iir_coeffs_mid[4] = -0.906562;
+
+        iir_coeffs_highmid[0] = 0.999999f;
+        iir_coeffs_highmid[1] = -1.787234f;
+        iir_coeffs_highmid[2] = 0.822248f;
+        iir_coeffs_highmid[3] = 1.787234f;
+        iir_coeffs_highmid[4] = -0.822248;
+
+        iir_coeffs_high[0] = 0.999999f;
+        iir_coeffs_high[1] = -1.216444f;
+        iir_coeffs_high[2] = 0.533295f;
+        iir_coeffs_high[3] = 1.216444f;
+        iir_coeffs_high[4] = -0.533295;
+
+        arm_biquad_cascade_df1_init_f32 ( &iirsettings_l, 1, &iir_coeffs_low[0], &iir_l_state[0]);
+        arm_biquad_cascade_df1_init_f32 ( &iirsettings_r, 1, &iir_coeffs_low[0], &iir_r_state[0]);
+        arm_biquad_cascade_df1_init_f32 ( &iirsettings_l, 1, &iir_coeffs_lowmid[0], &iir_l_state[0]);
+        arm_biquad_cascade_df1_init_f32 ( &iirsettings_r, 1, &iir_coeffs_lowmid[0], &iir_r_state[0]);
+        arm_biquad_cascade_df1_init_f32 ( &iirsettings_l, 1, &iir_coeffs_mid[0], &iir_l_state[0]);
+        arm_biquad_cascade_df1_init_f32 ( &iirsettings_r, 1, &iir_coeffs_mid[0], &iir_r_state[0]);
+        arm_biquad_cascade_df1_init_f32 ( &iirsettings_l, 1, &iir_coeffs_highmid[0], &iir_l_state[0]);
+        arm_biquad_cascade_df1_init_f32 ( &iirsettings_r, 1, &iir_coeffs_highmid[0], &iir_r_state[0]);
+        arm_biquad_cascade_df1_init_f32 ( &iirsettings_l, 1, &iir_coeffs_high[0], &iir_l_state[0]);
+        arm_biquad_cascade_df1_init_f32 ( &iirsettings_r, 1, &iir_coeffs_high[0], &iir_r_state[0]);
+        printf("Coefficients reset!\n");
+    } else {
+        printf("Invalid command\n");
+    }
+}
+
+void HAL_I2SEx_TxRxHalfCpltCallback(I2S_HandleTypeDef *hi2s){
+  callback_state = 1;
+}
+
+void HAL_I2SEx_TxRxCpltCallback(I2S_HandleTypeDef *hi2s){
+  callback_state = 2;
+}
 
 /* USER CODE END 4 */
 
